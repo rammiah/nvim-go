09e22001b6b8aec2ddfa382a6669c594ff7c0bb9
rammiah <rammiahcn@gmail.com>
2022-06-07 01:38:30 +0800

    feat(format): use diff output to update buffer

--- lua/go/format.lua
+++ lua/go/format.lua
@@ -1,10 +1,9 @@
 local M = {}
 
-local vim = vim
-local config = require('go.config')
-local system = require('go.system')
-local output = require('go.output')
-local util = require('go.util')
+local config = require("go.config")
+local system = require("go.system")
+local output = require("go.output")
+local util = require("go.util")
 
 function M.format(fmt)
     if not config.is_set(config.options.formatter) then
@@ -17,19 +16,6 @@ function M.format(fmt)
     return pcall(M[formatter])
 end
 
-local function arrayEqual(x, y)
-    if #x ~= #y then
-        return false
-    end
-    for k, v in pairs(x) do
-        if y[k] ~= v then
-            return false
-        end
-    end
-
-    return true
-end
-
 local function do_fmt(formatter, args)
     if not util.binary_exists(formatter) then
         return
@@ -39,25 +25,23 @@ local function do_fmt(formatter, args)
     -- goimports stdout result
     local result = vim.fn.systemlist(system.wrap_command(formatter, args), content)
     if vim.v.shell_error == 0 then
-        if not arrayEqual(content, result) then
-            vim.api.nvim_buf_set_lines(buf_nr, 0, -1, true, result)
-        end
-        output.show_success('GoFormat', 'Success')
+        util.apply_diffs(result, buf_nr)
+        output.show_success("GoFormat", "Success")
     else
-        output.show_error('GoFormat', 'error '..table.concat(result, '\n'))
+        output.show_error("GoFormat", "error " .. table.concat(result, "\n"))
     end
 end
 
 function M.gofmt()
-    do_fmt('gofmt', {})
+    do_fmt("gofmt", { "-d" })
 end
 
 function M.goimports()
-    do_fmt('goimports', { })
+    do_fmt("goimports", { "-d" })
 end
 
 function M.gofumpt()
-    do_fmt('gofumpt', { '-l' })
+    do_fmt("gofumpt", { "-l", "-d" })
 end
 
 return M
--- lua/go/util.lua
+++ lua/go/util.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 local vim = vim
-local output = require('go.output')
+local output = require("go.output")
 
 function M.current_word()
-    return vim.fn.expand('<cword>')
+    return vim.fn.expand("<cword>")
 end
 
 function M.current_line()
-    return vim.fn.getline('.')
+    return vim.fn.getline(".")
 end
 
 function M.binary_exists(bin)
@@ -16,8 +16,8 @@ function M.binary_exists(bin)
         return true
     end
     output.show_error(
-        'No Binary',
-        string.format('%s not exists. Run `:GoInstallBinaries`', bin)
+        "No Binary",
+        string.format("%s not exists. Run `:GoInstallBinaries`", bin)
     )
     return false
 end
@@ -26,11 +26,76 @@ function M.empty_output(data)
     if #data == 0 then
         return true
     end
-    if #data == 1 and data[1] == '' then
+    if #data == 1 and data[1] == "" then
         return true
     end
 
     return false
+end
+
+function M.slice(obj, start, finish)
+    if obj == nil or #obj == 0 or start == finish then return {} end
+
+    local output = {}
+    local _finish = #obj
+    local _start = 1
+
+    if start >= 0 then
+      _start = start
+    elseif finish == nil and start < 0 then
+      _start = #obj + start + 1
+    end
+
+    if (finish and finish >= 0) then
+      _finish = finish - 1
+    elseif finish and finish < 0 then
+      _finish = #obj + finish
+    end
+
+    for i = _start, _finish do
+      table.insert(output, obj[i])
+    end
+
+    return output
+end
+
+function M.apply_diffs(diffs, bufnr)
+    -- at least 3 lines with diff out header
+    if #diffs <= 3 then
+        return
+    end
+    local diffs = M.slice(diffs, 4)
+    -- parse diff into a replace table
+    local old_lines = {}
+    local new_lines = {}
+    local line = 0
+
+    for _, l in ipairs(diffs) do
+        if l:match("^@@") then
+            -- check before is a delete or append or replace
+            if #old_lines ~= 0 or #new_lines ~= 0 then
+                vim.api.nvim_buf_set_lines(bufnr, line, line + #old_lines, true, new_lines)
+            end
+            -- reset indexes to diff
+            line = l:match("@@ %-%d+,%d+ %+(%d+),%d+ @@")
+            line = tonumber(line) - 1 -- refer to line before current
+            new_lines = {}
+            old_lines = {}
+        elseif l:match("^%-") then
+            table.insert(old_lines, l:match("^%-(.*)$"))
+        elseif l:match("^%+") then
+            table.insert(new_lines, l:match("^%+(.*)$"))
+        elseif l:match("^ ") then
+            -- delete old lines, add new lines, replace lines
+            if #old_lines ~= 0 or #new_lines ~= 0 then
+                vim.api.nvim_buf_set_lines(bufnr, line, line + #old_lines, true, new_lines)
+                line = line + #new_lines
+                new_lines = {}
+                old_lines = {}
+            end
+            line = line + 1
+        end
+    end
 end
 
 return M
